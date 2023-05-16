592e845fc4a52cea11b95f7a79962d02a0a30cd6
roberto_farias <75376776+roberto06rhz@users.noreply.github.com>
2023-05-16 12:03:54 -0300

    tentei mas nao deu

--- App.js
+++ App.js
@@ -1,30 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import TelaPrincipalComponent from './Component/TelaPricipalComponent';
-import TelaLoginComponent from './Component/telaloginComponent';
-import TelaCadastroComponent from './Component/TelacadastroComponet';
-import TelaNoticiaComponent from './Component/TelaNoticiaComponent';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+// import {NavigationContainer} from '@react-navigation/native';
+// import TelaPrincipalComponent from './Component/TelaPricipalComponent';
+// import TelaLoginComponent from './Component/TelaLoginComponent';
+// import TelaCadastroComponent from './Component/TelacadastroComponet';
+// import TelaNoticiaComponent from './Component/TelaNoticiaComponent';
 
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const stack = createNativeStackNavigator();
 
-function app(){
+const App = () =>  {
 
   return(
-    <NavigationContainer>
-      <stack.navigation inicialRouterName = "">
-        <stack.Screen name = "" component = {TelaPrincipalComponent}></stack.Screen>
-        <stack.Screen name = "login" component = {TelaLoginComponent}></stack.Screen>
-        <stack.Screen name = "cadastro" component = {TelaCadastroComponent}></stack.Screen>
-        <stack.Screen name = "entrar como visitante" component = {TelaNoticiaComponent}></stack.Screen>
-      </stack.navigation>
-    </NavigationContainer>
+    // <NavigationContainer>
+    //   <stack.navigation inicialRouterName = "">
+    //     <stack.Screen name = "" component = {TelaPrincipalComponent}></stack.Screen>
+    //     <stack.Screen name = "login" component = {TelaLoginComponent}></stack.Screen>
+    //     <stack.Screen name = "cadastro" component = {TelaCadastroComponent}></stack.Screen>
+    //     <stack.Screen name = "entrar como visitante" component = {TelaNoticiaComponent}></stack.Screen>
+    //   </stack.navigation>
+    // </NavigationContainer>
+
+    <View> 
+
+    </View>
 
   )
 }
-export default app;
+export default App;
 
 // export default function App() {
 //   return (
